ef7ca3d16b1443b37957d36e64dc98e19e1f94c8
rohanraghuwanshi <rohanraghuvanshi001@gmail.com>
2021-03-20 23:20:18 +0530

    Typing effect added

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,32 @@
 function navbarToggle() {
     document.getElementById("navigationMenu").classList.toggle("expanded");
 }
+
+var typeText = document.querySelector(".typeText")
+var textToBeTyped = "Full Stack Developer"
+var index = 0, isAdding = true
+
+function playAnim() {
+  setTimeout(function () {
+    typeText.innerText = textToBeTyped.slice(0, index)
+    if (isAdding) {
+      if (index > textToBeTyped.length) {
+        isAdding = false
+        setTimeout(function () {
+          playAnim()
+        }, 2000)
+        return
+      } else {
+        index++
+      }
+    } else {
+      if (index === 0) {
+        isAdding = true
+      } else {
+        index--
+      }
+    }
+    playAnim()
+  }, 120)
+}
+playAnim()
