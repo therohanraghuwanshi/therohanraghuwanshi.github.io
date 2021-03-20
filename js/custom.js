9c0ceca6d522d7327a07058e47da6928633c60d1
rohanraghuwanshi <rohanraghuvanshi001@gmail.com>
2021-03-20 23:53:31 +0530

    Updates

--- js/custom.js
+++ js/custom.js
@@ -2,31 +2,54 @@ function navbarToggle() {
     document.getElementById("navigationMenu").classList.toggle("expanded");
 }
 
-var typeText = document.querySelector(".typeText")
-var textToBeTyped = "Full Stack Developer"
-var index = 0, isAdding = true
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
 
-function playAnim() {
-  setTimeout(function () {
-    typeText.innerText = textToBeTyped.slice(0, index)
-    if (isAdding) {
-      if (index > textToBeTyped.length) {
-        isAdding = false
-        setTimeout(function () {
-          playAnim()
-        }, 2000)
-        return
-      } else {
-        index++
-      }
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
     } else {
-      if (index === 0) {
-        isAdding = true
-      } else {
-        index--
-      }
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
     }
-    playAnim()
-  }, 120)
-}
-playAnim()
+
+    this.el.innerHTML = '<span class="wrap"> I am a '+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+};
